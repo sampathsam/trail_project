6ba8d17ab31db6b4ea6ac6c9415cd01850da7262
sampathsam <sam111arka@gmail.com>
2017-08-23 15:58:15 +0530

    Added Edit and Delete features

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,6 @@
 class PeopleController < ApplicationController
+  # before_action : find_person, only: [:show, :edit, :update]
+
   def index
     @people = Person.all.order("created_at DESC")
   end
@@ -20,7 +22,28 @@ end
 def show
   @person = Person.find(params[:id])
 end
-  private
+
+def edit
+  @person = Person.find(params[:id])
+end
+
+def update
+  @person = Person.find(params[:id])
+
+  if @person.update(person_params)
+    redirect_to @person
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @person = Person.find(params[:id])
+  @person.destroy
+  redirect_to root_path
+end
+
+private
 
   def person_params
     params.require(:person).permit(:Name, :Age)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :people
-root 'people#index'
+  root 'people#index'
 end
